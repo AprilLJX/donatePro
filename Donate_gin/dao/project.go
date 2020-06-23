f107295ff768dce4c27e771ebdda6aeec16ffba1
Jacyn <liujiaxin@pku.edu.cn>
2020-06-23 23:14:41 +0800

    更新获取项目列表api

---
+++ Donate_gin/dao/project.go
@@ -0,0 +1,37 @@
+package dao
+
+import (
+	"Donate_gin/db"
+	"Donate_gin/entity"
+	"fmt"
+	"log"
+)
+
+func GetProList()  (projectList []entity.DonaProject,err error){
+
+	projects := entity.DonaProject{
+
+	}
+	//返回if_end为0的项目
+	rows, err := db.DB.Query("SELECT * FROM dona_project WHERE if_end = 0")
+	defer rows.Close()
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	for rows.Next() {
+		if err1 := rows.Scan(&projects.ProId, &projects.DemandId, &projects.RecDonationNum, &projects.IfEnd); err1 != nil {
+			log.Fatal(err1)
+		}
+
+		projectList = append(projectList,projects)
+
+	}
+		return
+}
+
+func GetOnePro(demandId int)(onePro entity.RePro,err error)  {
+	err = db.DB.QueryRow("SELECT pro_name,introduction FROM demand_list WHERE demand_id=?",demandId).Scan(&onePro.ProName,&onePro.Introduction)
+	return
+}
---
+++ Donate_gin/models/project.go
@@ -0,0 +1,29 @@
+package models
+
+import (
+	"Donate_gin/dao"
+	"Donate_gin/entity"
+)
+
+func GetProList() (prolist []map[string]string,proNum int,err error){
+
+	onePro := entity.RePro{}
+
+
+	projectList,err := dao.GetProList()
+	for _,project := range projectList{
+		demandId := project.DemandId
+		onePro,err = dao.GetOnePro(demandId)
+		onePro.RecDonationNum = project.RecDonationNum
+
+		//将返回的单个项目列表用字典形式存储
+		oneProMap := make(map[string]string)
+		oneProMap["proName"] = onePro.ProName
+		oneProMap["proIntro"] = onePro.Introduction
+
+		prolist = append(prolist,oneProMap)
+	}
+	proNum = len(prolist)
+	return
+
+}
